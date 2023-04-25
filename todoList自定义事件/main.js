c63ed3144e385dd04eb664799a0db079b0e0face
dg <2438747008@qq.com>
2023-04-25 16:56:59 +0800

    message

---
+++ todoList自定义事件/main.js
@@ -0,0 +1,14 @@
+// 导入vue
+import Vue from "vue";
+
+// 导入APP组件
+import App from "./App.vue";
+
+// 关闭生产提示
+Vue.config.productionTip = false;
+
+// 创建vue实例对象
+new Vue({
+  el: "#app",
+  render: (h) => h(App),
+});
