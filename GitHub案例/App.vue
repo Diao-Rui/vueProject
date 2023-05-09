4e808bbe3b56d58abcccb7216795e01abf4f8570
dg <2438747008@qq.com>
2023-05-09 11:08:49 +0800

    message

---
+++ GitHub案例/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <!-- 定义一个容器 -->
+    <div class="container">
+        <!-- search组件 调用new VueComponent-->
+        <Search />
+        <!-- list组件 -->
+        <List />
+    </div>
+</template>
+
+<script>
+// 引入组件
+import Search from './components/search'
+import List from './components/list'
+export default {
+    name: 'App',
+    // 注册组件
+    components: { Search, List }
+}
+</script>
--- vue_test/src/App.vue
+++ vue_test/src/App.vue
@@ -1,20 +1,39 @@
 <template>
-    <!-- 定义一个容器 -->
-    <div class="container">
-        <!-- search组件 调用new VueComponent-->
-        <Search />
-        <!-- list组件 -->
-        <List />
-    </div>
+	<div class="container">
+		<Category title="美食">
+			<img src="https://s3.ax1x.com/2021/01/16/srJlq0.jpg" alt="">
+		</Category>
+
+		<Category title="游戏">
+			<ul>
+				<li v-for="(g, index) in games" :key="index">{{ g }}</li>
+			</ul>
+		</Category>
+
+		<Category title="电影">
+			<video controls src="http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4"></video>
+		</Category>
+	</div>
 </template>
 
 <script>
-// 引入组件
-import Search from './components/search'
-import List from './components/list'
+import Category from './components/Category'
 export default {
-    name: 'App',
-    // 注册组件
-    components: { Search, List }
+	name: 'App',
+	components: { Category },
+	data() {
+		return {
+			foods: ['火锅', '烧烤', '小龙虾', '牛排'],
+			games: ['红色警戒', '穿越火线', '劲舞团', '超级玛丽'],
+			films: ['《教父》', '《拆弹专家》', '《你好，李焕英》', '《尚硅谷》']
+		}
+	},
 }
 </script>
+
+<style scoped>
+.container {
+	display: flex;
+	justify-content: space-around;
+}
+</style>
