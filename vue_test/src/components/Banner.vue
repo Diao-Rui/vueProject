513214c82074e329b1597218112e3b3ab9b39401
dg <2438747008@qq.com>
2023-05-16 16:54:49 +0800

    message

--- vue_test/src/components/Banner.vue
+++ vue_test/src/components/Banner.vue
@@ -2,12 +2,30 @@
 	<div class="col-xs-offset-2 col-xs-8">
 		<div class="page-header">
 			<h2>Vue Router Demo</h2>
+			<button @click="back">后退</button>
+			<button @click="forward">前进</button>
+			<button @click="test">测试一下go</button>
 		</div>
 	</div>
 </template>
 
 <script>
 export default {
-	name: 'BannerView'
+	name: 'BannerView',
+	methods: {
+		back() {
+			// 栈顶指针向下移动一步
+			this.$router.back()
+		},
+		forward() {
+			// 栈顶指针向上移动一步
+			this.$router.forward()
+		},
+		go() {
+			//正数栈顶指针往上
+			//负数栈顶指针往下
+			this.$router.go(-1)
+		}
+	}
 }
 </script>
--- vue_test/src/pages/Message.vue
+++ vue_test/src/pages/Message.vue
@@ -2,20 +2,21 @@
 	<div>
 		<ul>
 			<li v-for="m in messageList" :key="m.id">
-				<!-- 跳转路由并携带query参数，to的字符串写法 -->
-				<!-- <router-link :to="`/home/message/detail/${m.id}&${m.title}`">{{m.title}}</router-link>&nbsp;&nbsp; -->
+				<!-- 跳转路由并携带params参数，to的字符串写法 -->
+				<!-- <router-link :to="`/home/message/detail/${m.id}/${m.title}`">{{m.title}}</router-link>&nbsp;&nbsp; -->
 
 				<!-- 跳转路由并携带params参数，to的对象写法 -->
 				<router-link :to="{
 					name: 'detail',
-					params: {
+					query: {
 						id: m.id,
 						title: m.title
 					}
 				}">
 					{{ m.title }}
 				</router-link>
-
+				<button @click="pushShow(m)">push查看</button>
+				<button @click="replaceShow(m)">replace查看</button>
 			</li>
 		</ul>
 		<hr>
@@ -33,6 +34,26 @@ export default {
 				{ id: '002', title: '消息002' },
 				{ id: '003', title: '消息003' }
 			]
+		}
+	},
+	methods: {
+		pushShow(m) {
+			this.$router.push({
+				name: 'detail',
+				params: {
+					id: m.id,
+					title: m.title
+				}
+			})
+		},
+		replaceShow(m) {
+			this.$router.replace({
+				name: 'detail',
+				params: {
+					id: m.id,
+					title: m.title
+				}
+			})
 		}
 	},
 }
